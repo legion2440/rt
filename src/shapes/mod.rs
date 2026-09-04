b5aa51c3adfa739c83961726350c76e761cf02c3
Nazar Yestayev <legion2440@mail.ru>
2026-09-04 23:09:45 +0500

    export particle and fluid bonus shapes

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -1,9 +1,13 @@
 pub mod cube;
 pub mod cylinder;
+pub mod fluid;
+pub mod particle;
 pub mod plane;
 pub mod sphere;
 
 pub use cube::Cube;
 pub use cylinder::Cylinder;
+pub use fluid::FluidSurface;
+pub use particle::ParticleCloud;
 pub use plane::Plane;
 pub use sphere::Sphere;
